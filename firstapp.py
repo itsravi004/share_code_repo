804848b0114fdf9b8cdab3572ccfd98b4001e619
itsravi004 <154854635+itsravi004@users.noreply.github.com>
2024-12-29 12:07:15 -0500

    streamit app for simple chat

--- firstapp.py
+++ firstapp.py
@@ -7,3 +7,23 @@ from langchain_core.prompts import ChatPromptTemplate, BaseChatPromptTemplate, C
 import os
 from dotenv import load_dotenv
 
+#langsmith Tracking
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT_ID"]="QandA ChatBot with OpenAI"
+
+#Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a AI coder proficient in python with SQL skills and an AI Assistant, please respond as requested"),
+        ("user","Question:{question}")
+    ]
+)
+
+def generate_response(question,api_key,llm,temperature,max_tokens):
+    openai.api_key = api_key
+    llm = ChatOpenAI(model=llm)
+    outputparser = StrOutputParser()
+    chain = prompt|llm|outputparser
+    answer = chain.invoke(question=question,temperature=temperature,max_tokens=max_tokens)
+    return answer
