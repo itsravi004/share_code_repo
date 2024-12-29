f83947263b53255c69614cc054c19d9645bb0958
itsravi004 <154854635+itsravi004@users.noreply.github.com>
2024-12-29 12:14:12 -0500

    streamit app for simple chat

--- firstapp.py
+++ firstapp.py
@@ -27,3 +27,24 @@ def generate_response(question,api_key,llm,temperature,max_tokens):
     chain = prompt|llm|outputparser
     answer = chain.invoke(question=question,temperature=temperature,max_tokens=max_tokens)
     return answer
+
+# Streamlit APP
+st.title("Q&A Chatbot with OpenAI")
+api_key = st.sidebar.text_input("Ender OpenAI API Key",type="password")
+# Dropdown for LLM Model
+llm = st.sidebar.selectbox("Select Open AI LLM Model",["gpt-3.5-turbo","gpt-4o-2024-11-20","gpt-4o-mini-2024-07-18","o1-mini-2024-09-12"])  
+
+#Adjust Temprature and Max Tokens
+temperature = st.sidebar.slider("Select Temprature",min_value=0.0,max_value=1.0,value=0.9,step=0.1)
+max_tokens = st.sidebar.slider("Select Max Tokens",min_value=50,max_value=500,value=200,step=50)
+
+# Main Chat Interface
+st.write("Ask your question")
+user_input = st.text_area("Enter your question here")
+
+if user_input:
+    response = generate_response(user_input,api_key,llm,temperature,max_tokens)
+    st.write(response)
+    st.write("By Ravi but Powered by OpenAI")
+else:
+    st.write("Please enter your question")
